17be52f4404c1f30016a50e3e6c02c0593053a39
Leigh Simpson <leigh.simpson@27partners.com>
2018-08-09 10:48:35 +0100

    Added test using React with Material UI

---
+++ react-material-ui/README.md
@@ -0,0 +1,11 @@
+# react-bootstrap
+
+Get as far as you building the layout provided in `mockup.pdf` using the React
+Material UI wrappers.
+
+## Build steps
+
+```bash
+yarn install
+yarn start
+```
---
+++ react-material-ui/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const App = () => (
+  <Button variant="contained" color="primary">
+    App goes here
+  </Button>
+);
+
+export default App;
