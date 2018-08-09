fb32a79abc6181d296f3efae373b100d1e609d47
Leigh Simpson <leigh.simpson@27partners.com>
2018-08-09 10:24:48 +0100

    Added test using React with SUI

---
+++ react-semantic-ui/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+const App = () => (
+  <Container>
+    <Header>App goes here</Header>
+  </Container>
+);
+
+export default App;
